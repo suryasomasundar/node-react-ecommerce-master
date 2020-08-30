0de03656ab14c62b0c55cb3fa685853a27973355
Somu <ssuryanarayanan@myvest.com>
2020-08-29 23:54:12 -0700

    YET-5:E2E-Core-Commit

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Kids-Dress-1',
+      category: 'Boys',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Kids-Dress-2',
+      category: 'Girls',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      name: 'Kids-Dress-3',
+      category: 'Girls',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    }, {
+      _id: '4',
+      name: 'Kids-Dress-4',
+      category: 'Boys',
+      image: '/images/d1.jpg',
+      price: 70,
+      brand: ' Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 6,
+    },
+  ]
+}
